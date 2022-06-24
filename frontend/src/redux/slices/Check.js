23f69eebc1fbdcb1e7c09da667fc488ce0f1a4df
latttysh <semyon.latysh@gmail.com>
2022-06-24 18:10:30 +0300

    axios redux logic

---
+++ frontend/src/redux/slices/Check.js
@@ -0,0 +1,38 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "../../axios";
+
+export const fetchTrack = createAsyncThunk(
+  "track/fetchTrack",
+  async (params) => {
+    console.log("Отправляем данные");
+    console.log(params);
+    const {data} = await axios.get("/track/")
+  }
+);
+
+const initialState = {
+  data: [],
+  status: "Loading",
+};
+
+const checkTrack = createSlice({
+  name: "track",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchTrack.pending]: (state) => {
+        state.data = null;
+        state.status = 'loading';
+      },
+      [fetchTrack.fulfilled]: (state, action) => {
+        state.data = action.payload;
+        state.status = 'loaded';
+      },
+    [fetchTrack.rejected]: (state) => {
+      state.data = null;
+      state.status = "error";
+    },
+  },
+});
+
+export const trackReduce = checkTrack.reducer;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import CalculateSlice from './slices/CalculateSlice';
+import {trackReduce} from "./slices/Check"
 
 export const store = configureStore({
   reducer: {
     info: CalculateSlice,
+    track: trackReduce
   },
 });
