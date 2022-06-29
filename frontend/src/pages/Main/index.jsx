5751d093bcc2d77de67638042a5ebb3239a8c875
latttysh <semyon.latysh@gmail.com>
2022-06-29 18:25:10 +0300

    adaptive main

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import styles from "./main.module.scss";
-import ServiceBlock from "../../components/ServiceBlock";
-import { Link } from "react-router-dom";
+import React from 'react';
+import styles from './main.module.scss';
+import ServiceBlock from '../../components/ServiceBlock';
+import { Link } from 'react-router-dom';
 
 function Main() {
-  const [value, setValue] = React.useState("");
-
+  const [value, setValue] = React.useState('');
   const onChangeTrack = (event) => {
     setValue(event.target.value);
   };
@@ -13,19 +12,16 @@ function Main() {
     <div className={styles.container}>
       <div className={styles.hero}>
         <div className={styles.hero__info}>
-          <div className={styles.hero__uptitle}>
-            Сибирская транспортная компания
-          </div>
+          <div className={styles.hero__uptitle}>Сибирская транспортная компания</div>
           <div className={styles.hero__title}>
             Доставка грузов <br />
             во все регионы России и за границу!
           </div>
           <div className={styles.hero__description}>
-            Доставка грузов специалистами в области международной
-            экспресс-доставки
+            Доставка грузов специалистами в области международной экспресс-доставки
           </div>
-          <Link to={"/calculate"}>
-          <button className={styles.hero__button}>Оформить доставку</button>
+          <Link to={'/calculate'}>
+            <button className={styles.hero__button}>Оформить доставку</button>
           </Link>
         </div>
         <div className={styles.hero__advantages}>
@@ -38,19 +34,15 @@ function Main() {
                 <div className={styles.info__text}>Отправлено посылок</div>
               </div>
             </div>
-            <div className={styles.hero__item} style={{ marginLeft: "5vw" }}>
+            <div className={styles.hero__item} style={{ marginLeft: '5vw' }}>
               <img className={styles.item__img} src="./img/human.png" alt="" />
               <div className={styles.hero__item__info}>
                 <div className={styles.info__counter}>321</div>
                 <div className={styles.info__text}>Довольных клиентов</div>
               </div>
             </div>
-            <div className={styles.hero__item} style={{ marginLeft: "12vw" }}>
-              <img
-                className={styles.item__img}
-                src="./img/indicator.png"
-                alt=""
-              />
+            <div className={styles.hero__item} style={{ marginLeft: '12vw' }}>
+              <img className={styles.item__img} src="./img/indicator.png" alt="" />
               <div className={styles.hero__item__info}>
                 <div className={styles.info__counter}>3 645 400</div>
                 <div className={styles.info__text}>Километров в пути</div>
@@ -63,12 +55,8 @@ function Main() {
         <div className={styles.search__block}>
           <div className={styles.search__title}>Отследить</div>
           <div className={styles.search}>
-            <input
-              onChange={onChangeTrack}
-              placeholder="Трек-номер отправления..."
-              type="text"
-            />
-            <Link to={"/check"} state={{ track: value }}>
+            <input onChange={onChangeTrack} placeholder="Трек-номер отправления..." type="text" />
+            <Link to={'/check'} state={{ track: value }}>
               <img src="./img/search.png" alt="" />
             </Link>
           </div>
@@ -81,8 +69,8 @@ function Main() {
           <div className={styles.calculate__reciever}>
             <input placeholder="Город получателя" type="text" />
           </div>
-          <Link to={"/calculate"}>
-          <button className={styles.calculate__btn}>Рассчитать</button>
+          <Link to={'/calculate'}>
+            <button className={styles.calculate__btn}>Рассчитать</button>
           </Link>
         </div>
       </div>
@@ -92,43 +80,43 @@ function Main() {
         <div className={styles.service__blocks}>
           <ServiceBlock
             text={`Наша транспортная компания доставляет по всей России без ограничений. Вам нужно всего лишь указать город.`}
-            title={"Доставка по России"}
-            img={"rocket"}
-            width={"35%"}
+            title={'Доставка по России'}
+            img={'rocket'}
+            width={'35%'}
           />
           <ServiceBlock
             text={`Самый распространенный способ перевозки грузов. Никакие другие транспортные средства не имеют сопоставимой инфраструктуры`}
-            title={"Перевозка грузовыми машинами"}
-            img={"car"}
-            width={"35%"}
-            marginTop={"-30px"}
+            title={'Перевозка грузовыми машинами'}
+            img={'car'}
+            width={'35%'}
+            marginTop={'-30px'}
           />
           <ServiceBlock
             text={`Вам нужно отправить посылку в другую страну? Без проблем. Укажите точный адрес и мы с вами свяжемся.`}
-            title={"Международная доставка"}
-            img={"planet"}
-            width={"45%"}
-            marginTop={"-30px"}
+            title={'Международная доставка'}
+            img={'planet'}
+            width={'45%'}
+            marginTop={'-30px'}
           />
           <ServiceBlock
             text={`Железнодорожный транспорт отличается высокой грузоподъемностью и скоростью доставки`}
-            title={"Перевозка контейнером"}
-            img={"container"}
-            width={"30%"}
-            marginTop={"-30px"}
+            title={'Перевозка контейнером'}
+            img={'container'}
+            width={'30%'}
+            marginTop={'-30px'}
           />
           <ServiceBlock
             text={`В нашем штате работает более 400 курьеров по всему миру. Ваша посылка дойдёт прям до двери.`}
-            title={"Курьерская доставка"}
-            img={"handes"}
-            width={"40%"}
-            marginTop={"20px"}
+            title={'Курьерская доставка'}
+            img={'handes'}
+            width={'40%'}
+            marginTop={'20px'}
           />
           <ServiceBlock
             text={`Надежные и прочные универсальные тары могут транспортироваться службами разных стран.`}
-            title={"Перевозка грузовым поездом"}
-            img={"rocket"}
-            width={"35%"}
+            title={'Перевозка грузовым поездом'}
+            img={'rocket'}
+            width={'35%'}
           />
         </div>
       </div>
@@ -175,12 +163,12 @@ function Main() {
         </div>
       </div>
 
-      <div className={styles.safe}>
+      {/* <div className={styles.safe}>
         <div className={styles.safe__title}>Безопасная сделка</div>
         <div className={styles.safe__subtitle}>
-          Для новых пользователей у нас есть услуга "Безопасная сделка".
-          Сибирская транспортная компания выступает в роли третьего
-          лица(посредника) в сделке между покупателем и продавцом.
+          Для новых пользователей у нас есть услуга "Безопасная сделка". Сибирская транспортная
+          компания выступает в роли третьего лица(посредника) в сделке между покупателем и
+          продавцом.
         </div>
 
         <div className={styles.safe_levels}>
@@ -188,67 +176,64 @@ function Main() {
             <div className={styles.level__title}>Договоренность участников</div>
             <div className={styles.counter}>1</div>
             <div className={styles.level__description}>
-              После того, как покупатель и продавец обсудили условия сделки,
-              продавец вызывает курьера нашей компании.
+              После того, как покупатель и продавец обсудили условия сделки, продавец вызывает
+              курьера нашей компании.
             </div>
           </div>
           <div className={styles.level}>
             <div className={styles.level__title}>Договор и проверка</div>
             <div className={styles.counter}>2</div>
             <div className={styles.level__description}>
-              Курьер составляет договор с описанием товара, проверяет его на
-              наличие скрытых дефектов и повреждений.
+              Курьер составляет договор с описанием товара, проверяет его на наличие скрытых
+              дефектов и повреждений.
             </div>
           </div>
           <div className={styles.level}>
             <div className={styles.level__title}>Трек-номер</div>
             <div className={styles.counter}>3</div>
             <div className={styles.level__description}>
-              После заключения договора с отправителем о предоставлении услуг
-              нашей компании, курьер забирает посылку у отправителя, присваивая
-              ей в системе трек-номер для отслеживания
+              После заключения договора с отправителем о предоставлении услуг нашей компании, курьер
+              забирает посылку у отправителя, присваивая ей в системе трек-номер для отслеживания
             </div>
           </div>
           <div className={styles.level}>
             <div className={styles.level__title}>Безопасная оплата</div>
             <div className={styles.counter}>4</div>
             <div className={styles.level__description}>
-              Покупателю необходимо связаться с оператором в чате для получения
-              реквизитов оплаты услуги "Безопасной сделки". Денежные средства
-              зарезервируются на счету компании до тех пор, пока получатель не
-              подтвердит получение им товара.
+              Покупателю необходимо связаться с оператором в чате для получения реквизитов оплаты
+              услуги "Безопасной сделки". Денежные средства зарезервируются на счету компании до тех
+              пор, пока получатель не подтвердит получение им товара.
             </div>
           </div>
           <div className={styles.level}>
             <div className={styles.level__title}>Осмотр посылки</div>
             <div className={styles.counter}>5</div>
             <div className={styles.level__description}>
-              После доставки курьером посылки получателю, получатель имеет право
-              осмотра и проверки посылки в течение 1 часа.
+              После доставки курьером посылки получателю, получатель имеет право осмотра и проверки
+              посылки в течение 1 часа.
             </div>
           </div>
           <div className={styles.level}>
             <div className={styles.level__title}>Возврат средств</div>
             <div className={styles.counter}>6</div>
             <div className={styles.level__description}>
-              При положительном результате осмотра курьер отмечает в системе
-              доставку груза и система автоматически переводит денежные средства
-              отправителю. При отрицательном результате осмотра, т.е. если
-              получатель отказывается от забора посылки, составляется акт
-              возврата денежных средств получателю и система делает
-              автоматический возврат денежных средств в течение 10 минут.
+              При положительном результате осмотра курьер отмечает в системе доставку груза и
+              система автоматически переводит денежные средства отправителю. При отрицательном
+              результате осмотра, т.е. если получатель отказывается от забора посылки, составляется
+              акт возврата денежных средств получателю и система делает автоматический возврат
+              денежных средств в течение 10 минут.
             </div>
           </div>
           <div className={styles.level}>
             <div className={styles.level__title}>Возврат посылки</div>
             <div className={styles.counter}>7</div>
             <div className={styles.level__description}>
-              Посылка возвращается назад отправителю, сумма доставки, оплаченной
-              отправителем, возврату не подлежит.
+              Посылка возвращается назад отправителю, сумма доставки, оплаченной отправителем,
+              возврату не подлежит.
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
